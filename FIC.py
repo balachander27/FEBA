0b1dcefebd693809d5e38ecf8ffdc0f4af136cd4
Prahalad Srinivas C G <32165701+cgprahalad@users.noreply.github.com>
2021-05-20 15:54:08 +0530

    Update FIC.py

--- FIC.py
+++ FIC.py
@@ -3,56 +3,67 @@ Authors: Balachander S, Prahalad Srinivas C G, Yogesh Chandra Singh Samant, B Va
 '''
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import datasets
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.svm import SVC
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
 
 class FeatureClassifier:
-  def __init__(self,reqAcc=0.80,classifier='DesicionTree',bias0=0,bias1=0,depth=None):
-    self.featureClassifiers=[]
-    self.reqAcc=reqAcc
-    self.indexLs=[]
+  def __init__(self,reqAcc=0.01,classifier='DesicionTree',bias=[],control=None,n_jobs=None,random_state=None):
+    self.featureClassifiers=[] #list of all the classifiers of all the selected features
+    self.reqAcc=reqAcc #user specified cutoff value
+    self.indexLs=[] # list of mapped index values to fetureClassifiers
     self.flag=0
-    self.bias0=bias0
-    self.bias1=bias1
-    self.depth=depth
-    self.classifier=classifier
-    self.dic={'DesicionTree':0,'LinearRegression':1,'SVM':2,'LogisticRegression':3}
+    self.bias=bias # list of biases for each and every label
+    self.control=control #overfitting control for desicion trees
+    self.classifier=classifier #the classifier which is preferred by the user
+    self.dic={'DesicionTree':0,'LinearRegression':1,'SVM':2,'LogisticRegression':3} #a dictionary which maps the classifier to its index
+    self.n_jobs=n_jobs
+    self.random_state=random_state
+    self.num_lables = None
+
   def finIndex(self):
+    #finds the index where the reqAcc condition fails and also created the indexLs[] for mapping
     for i in range(len(self.featureClassifiers)):
       if self.featureClassifiers[i][1] < self.reqAcc:
         return i
       self.indexLs.append(self.featureClassifiers[i][2])
     self.flag=1
     return i
+
   def fit(self,x,y):
+    #applied the model to the dataset. The model is trained and saved for further prediction
+    self.num_lables=len(set(y.flatten()))
+    bestfeatures = SelectKBest(score_func=chi2,k=1)
+    fit = bestfeatures.fit(x,y)
     for i in range(len(x[0])):
-      clf=[DecisionTreeClassifier(),LinearRegression(),SVC(), LogisticRegression()][self.dic[self.classifier]]
-      X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
+      clf=[DecisionTreeClassifier(max_depth=self.control,random_state=self.random_state),LinearRegression(n_jobs=self.n_jobs),SVC(gamma=self.control,random_state=self.random_state), LogisticRegression(penalty=self.control,random_state=self.random_state)][self.dic[self.classifier]]
+      X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33,random_state=self.random_state)
       clf.fit(X_train[:,i:i+1],y_train)
-      self.featureClassifiers.append((clf,accuracy_score(y_test,np.round(clf.predict(X_test[:,i:i+1]))),i))
+      self.featureClassifiers.append((clf,fit.scores_[i],i))
     self.featureClassifiers.sort(key=lambda x:x[1],reverse=True)
     index=self.finIndex()
     if self.flag==0:
       self.featureClassifiers=self.featureClassifiers[:index]
-    else:
-      pass
     return
+
   def predict(self,x):
-    class0=0
-    class1=1
+    #given a list of inputs, predicts the possible outputs
+    if not self.bias:
+      self.bias=np.zeros(self.num_lables)
+    if len(self.bias)<self.num_lables:
+      raise AttributeError('Please check the lenth of bias list')
     yPred=[]
     for i in range(len(x)):
-      class0=0
-      class1=0
+      pred_arr=np.zeros(self.num_lables)
       for j in range(len(self.indexLs)):
-        pred=self.featureClassifiers[j][0].predict([[x[i][self.indexLs[j]]]])
-        if pred == 0:
-          class0+=self.featureClassifiers[j][1]+self.bias0
-        else:
-          class1+=self.featureClassifiers[j][1]+self.bias1
-      yPred.append(1 if class1>class0 else 0)
+        pred=np.round(self.featureClassifiers[j][0].predict([[x[i][self.indexLs[j]]]]))
+        pred_arr[pred]+=self.featureClassifiers[j][1]+self.bias[pred[0]]
+      yPred.append(np.argmax(pred_arr))
     return yPred
